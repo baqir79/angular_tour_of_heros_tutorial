3d51eb19b7c5e2a7b9922e0eae951f09d1241a76
Mohammed Quaziar <mbaquirq@gmail.com>
2018-06-09 06:32:35 -0400

    finished hero detail

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -11,5 +11,7 @@ export class HeroDetailComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  @Input() hero: Hero;
 
 }
